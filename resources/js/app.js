5de7ba93bf052fd920a34e4fec7f98db75d19e58
Caio Henrique <caiohenrique@jeraapps.com>
2021-11-30 10:50:25 -0400

    [NEW] add bootstrap vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,15 @@
-import Vue from 'vue'
-import { createInertiaApp } from '@inertiajs/inertia-vue'
+import Vue from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 createInertiaApp({
   resolve: name => require(`./Pages/${name}`),
   setup({ el, App, props }) {
     new Vue({
       render: h => h(App, props),
-    }).$mount(el)
+    }).$mount(el);
   },
-})
+});
